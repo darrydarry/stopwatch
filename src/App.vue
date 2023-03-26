fa6c2dc1b1fe9e9643a6885265ba0def72eb9c2f
DarryDarry <da.ershova@mail.ru>
2023-03-26 20:59:57 +0300

    fix: format files

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,10 @@
 <script setup>
-import { ref } from 'vue';
-import StopwatchComponent from './components/StopwatchComponent.vue';
+import { ref } from 'vue'
+import StopwatchComponent from './components/StopwatchComponent.vue'
 const stopWatchers = ref([])
 const uniqId = ref(0)
 const removeStopwatch = (id) => {
-  const newstopWatchers = stopWatchers.value.filter(el => el.id !== id)
+  const newstopWatchers = stopWatchers.value.filter((el) => el.id !== id)
   console.log(newstopWatchers)
   stopWatchers.value = newstopWatchers
 }
@@ -19,14 +19,15 @@ const addStopwatch = () => {
     <div class="watches-wrapp">
       <div class="">
         <div class="wrapp">
-
-
-          <StopwatchComponent v-for="stopWatcher in stopWatchers" :key="stopWatcher.id" class="stop-watch"
-            @removeStopwatch="() => removeStopwatch(stopWatcher.id)" />
+          <StopwatchComponent
+            v-for="stopWatcher in stopWatchers"
+            :key="stopWatcher.id"
+            class="stop-watch"
+            @removeStopwatch="() => removeStopwatch(stopWatcher.id)"
+          />
           <div class="square plus-wrap" @click="() => addStopwatch()">
             <div class="plus" />
           </div>
-
         </div>
       </div>
     </div>
@@ -51,15 +52,14 @@ const addStopwatch = () => {
   margin-right: 50px;
 }
 
-
 @media screen and (min-width: 1024px) {
-  .stop-watch:nth-child(3n+3) {
+  .stop-watch:nth-child(3n + 3) {
     margin-right: 0;
   }
 }
 
 @media screen and (min-width: 768px) and (max-width: 1023px) {
-  .stop-watch:nth-child(2n+2) {
+  .stop-watch:nth-child(2n + 2) {
     margin-right: 0;
   }
 
@@ -97,7 +97,7 @@ const addStopwatch = () => {
 
   width: 20px;
   height: 20px;
-  background: linear-gradient(to bottom, transparent 35%, #9E9E9E 35%, #9E9E9E 65%, transparent 65%), linear-gradient(to right, transparent 35%, #9E9E9E 35%, #9E9E9E 65%, transparent 65%);
+  background: linear-gradient(to bottom, transparent 35%, #9e9e9e 35%, #9e9e9e 65%, transparent 65%),
+    linear-gradient(to right, transparent 35%, #9e9e9e 35%, #9e9e9e 65%, transparent 65%);
 }
 </style>
-
